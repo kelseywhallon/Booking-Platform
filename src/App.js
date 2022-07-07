c188bbc1d16a0d8d37bf2d1f1edb4d8642ecb9f0
kelseywhallon <kelsey.whallon@gmail.com>
2022-07-07 15:19:11 -0400

    fixes route and switch errors by changing router dom to version 5, adds components directory and adds navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
 
+import Home from './pages/Home';
+import Rooms from './pages/Rooms';
+import SingleRoom from './pages/SingleRoom';
+import Error from './pages/Error';
+import Navbar from './components/Navbar';
 
+import { Route, Switch, Link } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      Hello from app!
+      <Navbar />
+      <Switch> 
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms/" component={Rooms} />
+        <Route exact path="/rooms/:roomtype" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
     </>
   );
 }
