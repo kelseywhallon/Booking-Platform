fcc64d2e2a29bf2a2fdad555317dd448a8499c40
kelseywhallon <kelsey.whallon@gmail.com>
2022-07-07 18:35:59 -0400

    adds banner, services, and title components

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
+import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import Services from "../components/Services"
 
 export default function Home() {
     return (
-        <div>Hello from Home Page! </div>
+        //trying to render two adjacent components can be solved with react fragment or 2 angle brackets
+        <>
+        <Hero>
+            <Banner 
+                title="luxurious rooms" 
+                subtitle="deluxe rooms starting at $299" >
+                <Link to="/rooms" className="btn-primary">
+                    Our Rooms
+                </Link> 
+            </Banner>
+        </Hero>
+        <Services />
+        </>
+        
     )
 }
--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,7 +1,16 @@
 import React from 'react'
+import Hero from "../components/Hero"
+import Banner from '../components/Banner'
+import {Link} from 'react-router-dom';
 
 export default function Rooms() {
   return (
-    <div>Hello from the Rooms Page! </div>
+    <Hero hero="roomsHero">
+      <Banner title="Our Rooms" >
+        <Link to='/' className="btn-primary">
+          Return Home
+        </Link>
+      </Banner>
+    </Hero>
   )
 }
