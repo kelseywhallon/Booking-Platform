5ed809b69cd0b6401787b74f9d6bee67bf86ac0b
kelseywhallon <kelsey.whallon@gmail.com>
2022-07-11 15:21:46 -0400

    adds featured rooms

---
+++ src/components/FeaturedRooms.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class FeaturedRooms extends Component {
+    render() {
+        return (
+            <div>Hello from Featured Rooms</div>
+        )
+    }
+}
